172b42b47c89a1ebc002b1ab94d7c8fbcb6e4beb
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2025-03-30 16:08:42 +0200

    Changed path

--- Inventory-System/Assets/Scripts/Example/InventorySystem/Configs/Editor/ConfigImportsMenu.cs
+++ Inventory-System/Assets/Scripts/Example/InventorySystem/Configs/Editor/ConfigImportsMenu.cs
@@ -9,13 +9,13 @@ namespace Example.InventorySystem.Configs.Editor
         private const string ITEMS_SHEET_NAME = "InventoryItems";
         private const string CREDITS_NAME = "inventorysystem-434520-4c0545b0de51.json";
         private const string SHEET_ID = "1wXHYN_RUZPmRagWtJmsx3uC8D7I81f0jgoSTnwycACM";
-        private const string GAME_SETTINGS_DATA = "Assets/Data/Game Settings.asset";
+        private const string GAME_SETTINGS_DATA = "Assets/Resources/Data/Game Settings.asset";
 
         [MenuItem("GoggleImporter/Import Inventory System")]
         public static async void LoadItemsSettings()
         {
             var sheetsImporter = new GoogleSheetsImporter(CREDITS_NAME, SHEET_ID);
-
+            
             var gameSettings = AssetDatabase.LoadAssetAtPath<GameSettings>(GAME_SETTINGS_DATA);
             var itemsParser = new ItemDataParser<ItemParsableData>(gameSettings.Items);
             await sheetsImporter.DownloadAndParseSheetAsync(ITEMS_SHEET_NAME, itemsParser, 2);
